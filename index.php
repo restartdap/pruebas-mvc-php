e7560ff6ee9d8acd2da551b7a1d4dbc9b3fe6d9a
Rafael Estrada <restradap1600@gmail.com>
2020-11-30 17:28:27 -0600

    Se importaron las librerias internas

--- index.php
+++ index.php
@@ -1,7 +1,8 @@
 <?php
+    require_once "libs/controller.php";
+    require_once "libs/view.php";
+    require_once "libs/model.php";
     require_once "libs/app.php";
 
     $app = new App();
-
-    // echo "Estás en index.html";
 ?>

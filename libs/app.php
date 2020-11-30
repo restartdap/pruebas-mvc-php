63de24f14a11d72d4de4c9b88283e0f16888098e
Rafael Estrada <restradap1600@gmail.com>
2020-11-30 01:16:35 -0600

    Se manejaron los controladores Errores y Main en app.php

--- libs/app.php
+++ libs/app.php
@@ -1,10 +1,35 @@
 <?php
 
-class App {
-    function __construct()
-    {
+require_once "controllers/errores.php";
+
+class App{
+    function __construct() {
         echo "<p>Nueva App</p>";
+
+        $url = $_GET["url"];
+        echo "<p>{$url}</p>";
+
+        $url = rtrim($url, '/');
+        $url = explode('/', $url);
+        print_r($url);
+
+        $controller_name = $url[0];
+
+        $archivo_controller = "controllers/{$controller_name}.php";
+
+        if(file_exists($archivo_controller)) {
+
+            require_once $archivo_controller;
+            $controller = new $controller_name();
+
+            if(isset($url[1])) {
+
+                $method = $url[1];
+                $controller->{$method}();
+            }
+        }
+        else {
+            $controller = new Errores();
+        }
     }
 }
-
-?>

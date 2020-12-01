db2b796692072ea40ff354e9d5029c7f0a89bc66
Rafael Estrada <restradap1600@gmail.com>
2020-12-01 16:24:41 -0600

    Se añadió el archivo config.php

---
+++ config/config.php
@@ -0,0 +1,8 @@
+<?php
+
+define("URL", "http://localhost/aplicacion-mvc-php");
+define("HOST", "localhost");
+define("DB", "");
+define("USER", "root");
+define("PASSWORD", "");
+define("CHARSET", "utf8mb4");

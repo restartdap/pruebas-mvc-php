58b2063c082ec02ff314b3816344bd92da7b2c87
Rafael Estrada <restradap1600@gmail.com>
2020-12-01 18:41:28 -0600

    Se añadió una función al controlador Nuevo

--- controllers/nuevo.php
+++ controllers/nuevo.php
@@ -6,4 +6,15 @@ class Nuevo extends Controller {
         parent::__construct();
         $this->view->render("nuevo/index");
     }
+
+    function registrarAlumno() {
+
+        $matricula = $_POST["matricula"];
+        $nombre = $_POST["nombre"];
+        $apellido = $_POST["apellido"];
+
+        $this->model->insert();
+
+        echo "El modelo se ha insertado";
+    }
 }

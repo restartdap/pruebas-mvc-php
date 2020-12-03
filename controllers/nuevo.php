7e67f24851bb27e88f1761cabb1e5bb6b1ad2e07
Rafael Estrada <restradap1600@gmail.com>
2020-12-02 20:02:52 -0600

    Se desarrolló el metodo registrarAlumnos del controlador Nuevo

--- controllers/nuevo.php
+++ controllers/nuevo.php
@@ -4,17 +4,35 @@ class Nuevo extends Controller {
 
     function __construct() {   
         parent::__construct();
+    }
+
+    function render() {
         $this->view->render("nuevo/index");
     }
 
     function registrarAlumno() {
 
-        $matricula = $_POST["matricula"];
-        $nombre = $_POST["nombre"];
-        $apellido = $_POST["apellido"];
+        $mensaje = "";
 
-        $this->model->insert();
+        if(count($_POST) == 3) {
+            $matricula = $_POST["matricula"];
+            $nombre = $_POST["nombre"];
+            $apellido = $_POST["apellido"];
 
-        echo "El modelo se ha insertado";
+            $resultado = $this->model->insert(["matricula" => $matricula, "nombre" => $nombre, "apellido" => $apellido]);
+
+            if($resultado) {
+                $mensaje = "Nuevo alumno registrado";
+            }
+            else {
+                $mensaje = "No se pudo registrar al nuevo alumno";
+            }
+        }
+        else {
+            $mensaje = "No se enviaron valores";
+        }
+        
+        $this->view->mensaje = $mensaje;
+        $this->render();
     }
 }

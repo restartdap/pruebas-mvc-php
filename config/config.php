63692bdde24ae79c0ecc9e894146a31ee4a15167
Rafael Estrada <restradap1600@gmail.com>
2020-12-02 19:56:26 -0600

    Se cambió la URL para desarrollo y se agregó la base de datos

--- config/config.php
+++ config/config.php
@@ -1,8 +1,8 @@
 <?php
 
-define("URL", "https://morning-plateau-64057.herokuapp.com");
+define("URL", "http://localhost/aplicacion-mvc-php");
 define("HOST", "localhost");
-define("DB", "");
+define("DB", "mvc");
 define("USER", "root");
 define("PASSWORD", "");
 define("CHARSET", "utf8mb4");

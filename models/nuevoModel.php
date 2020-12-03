849409797f0d4106c2f5a37d6f6be87d28402d47
Rafael Estrada <restradap1600@gmail.com>
2020-12-02 19:55:11 -0600

    Se desarrollo la función de Insertar en NuevoModel

--- models/nuevoModel.php
+++ models/nuevoModel.php
@@ -5,7 +5,16 @@ class NuevoModel extends Model {
         parent::__construct();
     }
 
-    function insert() {
-        echo "Insertar datos";
+    function insert($datos) {
+
+        try {
+            $query = $this->db->connect()->prepare("INSERT INTO ALUMNOS(MATRICULA, NOMBRE, APELLIDO) VALUES(:matricula, :nombre, :apellido)");
+            $query->execute(["matricula" => $datos["matricula"], "nombre" => $datos["nombre"], "apellido" => $datos["apellido"]]);
+            return true;
+        } catch (PDOException $e) {
+            // echo $e->getMessage();
+            return false;
+        }
+
     }
 }

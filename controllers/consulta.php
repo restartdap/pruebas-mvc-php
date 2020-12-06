6c78a1421e81fa62a493393c72de498355daaffd
Rafael Estrada <restradap1600@gmail.com>
2020-12-05 22:15:45 -0600

    Se añadió codigo al controlador Consulta

--- controllers/consulta.php
+++ controllers/consulta.php
@@ -4,9 +4,12 @@ class Consulta extends Controller {
 
     function __construct() {   
         parent::__construct();
+        $this->view->alumnos = [];
     }
 
     function render() {
+        $alumnos = $this->model->get();
+        $this->view->alumnos = $alumnos;
         $this->view->render("consulta/index");
     }
 }

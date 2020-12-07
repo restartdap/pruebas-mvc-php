5d6983a0c6a419714fcca5bdef477cceebd38b49
Rafael Estrada <restradap1600@gmail.com>
2020-12-06 23:31:57 -0600

    Se añadió codigo a la vista del controlador de Consulta

--- views/consulta/index.php
+++ views/consulta/index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,17 +8,42 @@
     <link rel="stylesheet" href="<?php echo constant("URL") ?>/public/css/default.css">
     <link rel="stylesheet" href="<?php echo constant("URL") ?>/public/css/consulta.css">
 </head>
+
 <body>
     <?php require "views/header.php"; ?>
 
     <main class="main">
         <p class="title">Consulta</p>
+        <table>
+            <thead>
+                <tr>
+                    <th>Matricula</th>
+                    <th>Nombre</th>
+                    <th>Apellido</th>
+                </tr>
+            </thead>
 
-        <?php
-            var_dump($this->alumnos);
-        ?>
+            <tbody>
+                <?php
+                    include_once "models/alumno.php"; 
+                    foreach ($this->alumnos as $row) { 
+                    $alumno = new Alumno();
+                    $alumno = $row;
+                ?>
+                <tr>
+                    <td><?php echo $alumno->matricula; ?></td>
+                    <td><?php echo $alumno->nombre; ?></td>
+                    <td><?php echo $alumno->apellido; ?></td>
+                    <td><a href="<?php echo constant("URL")."/consulta/verAlumno/".$alumno->matricula ?>">Editar</a></td>
+                    <td><a href="<?php echo constant("URL")."/consulta/eliminarAlumno/".$alumno->matricula ?>">Eliminar</a></td>
+                </tr>
+
+                <?php } ?>
+            </tbody>
+        </table>
     </main>
 
     <?php require "views/footer.php"; ?>
 </body>
+
 </html>

5c3db0b8e7d8a6eb074b9f34ef831b5b80c89b37
Rafael Estrada <restradap1600@gmail.com>
2020-12-07 23:56:22 -0600

    Se desarrolló el metodo verAlumno y actualizarAlumno del controlador Consulta

--- controllers/consulta.php
+++ controllers/consulta.php
@@ -8,13 +8,42 @@ class Consulta extends Controller {
     }
 
     function render() {
-        $alumnos = $this->model->get();
+        $alumnos = $this->model->getAll();
         $this->view->alumnos = $alumnos;
         $this->view->render("consulta/index");
     }
 
-    function verAlumno() {
+    function verAlumno($param = null) {
+        $id_alumno = $param[0];
+        $alumno = $this->model->getById($id_alumno);
+        session_start();
+        $_SESSION["id_verAlumno"] = $alumno->matricula;
+        $this->view->alumno = $alumno;
+        $this->view->render("consulta/detalle");
+    }
 
+    function actualizarAlumno () {
+        session_start();
+        $matricula = $_SESSION["id_verAlumno"];
+        $nombre = $_POST["nombre"];
+        $apellido = $_POST["apellido"];
+
+        unset($_SESSION["id_verAlumno"]);
+
+        if($this->model->update(["matricula" => $matricula, "nombre" => $nombre, "apellido" => $apellido])) {
+            $alumno = new Alumno();
+            $alumno->matricula = $matricula;
+            $alumno->nombre = $nombre;
+            $alumno->apellido = $apellido;
+
+            $this->view->alumno = $alumno;
+            $this->view->mensaje = 1;
+        }
+        else {
+            $this->view->mensaje = 2;
+        }
+
+        $this->view->render("consulta/detalle");
     }
 
     function eliminarAlumno() {
